48bf6ea30fc0938fce6477c1a3d6b595e2fce8eb
gkp96 <gautam2002pandeyjee@gmail.com>
2024-06-08 12:56:26 +0530

    port added

--- Server.js
+++ Server.js
@@ -7,6 +7,7 @@ app.get("/main", () => {
     message: "Hello world!",
   });
 });
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
